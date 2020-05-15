31a1df8b7ae0d19e2699751bca6c744978cdd7b2
Lambda_School_Loaner_181 <lambda_school_loaner_181@Lambda-School-Loaner-181s-MacBook-Pro.local>
2020-05-14 20:15:00 -0500

    done

--- posts/postRouter.js
+++ posts/postRouter.js
@@ -1,27 +1,103 @@
+// const express = require("express");
+
+// const db = require("./postDb");
+
 const express = require('express');
+const db = require('./postDb');
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
+
   // do your magic!
+router.use("/:id", validatePostId);
+
+router.get('/', (req, res) => {
+  db.get()
+      .then(posts => {
+          res.status(200).json(posts);
+      })
+      .catch(err => {
+          res.status(500).json({
+              err,
+              errorMessage: "Can't retrieve posts."
+          });
+      });
 });
 
 router.get('/:id', (req, res) => {
-  // do your magic!
+  db.getById(req.params.id)
+      .then(post => {
+          res.status(200).json(post);
+      })
+      .catch(err => {
+          res.status(404).json({
+              err,
+              errorMessage: "Can't retrieve post."
+          });
+      });
 });
 
-router.delete('/:id', (req, res) => {
   // do your magic!
+  router.delete('/:id', (req, res) => {
+    db.remove(req.params.id)
+        .then(count => {
+            count === 1
+                ? res.status(200).json({
+                      message: `${count} post was removed from database.`
+                  })
+                : res.status(500).json({
+                      err,
+                      errorMessage: "Can't remove post."
+                });
+        })
+        .catch(err => {
+            res.status(500).json({
+                err,
+                errorMessage: "Can't remove post."
+            });
+        });
 });
 
-router.put('/:id', (req, res) => {
   // do your magic!
+  router.put('/:id', (req, res) => {
+    db.update(req.params.id, req.body)
+        .then(count => {
+            count === 1
+                ? res.status(200).json({
+                      message: `${count} post was updated in database.`
+                  })
+                : res.status(500).json({
+                      err,
+                      errorMessage: "Can't update post."
+                  });
+        })
+        .catch(err => {
+            res.status(500).json({
+                err,
+                errorMessage: "Can't update post."
+            });
+        });
 });
+
 
 // custom middleware
 
 function validatePostId(req, res, next) {
   // do your magic!
+  db.getById(req.params.id)
+        .then(post => {
+            post
+                ? next()
+                : res.status(400).json({
+                    message: "Invalid post id." 
+                });
+        })
+        .catch(err => {
+            res.status(500).json({
+                err,
+                message: "Error connecting to database."
+            });
+        });
 }
 
 module.exports = router;
